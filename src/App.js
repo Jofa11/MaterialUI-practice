02cff57941791d4a738f947d7488fe56e71ada7f
Joshua Favre <joshuaf@MacBook-Pro.local>
2021-02-04 12:49:35 -0600

    makeStyles for button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,24 @@ import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import TextField from '@material-ui/core/TextField';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B, #FF8E53)',
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: 'white',
+    padding: '5px 30px'
+  }
+})
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>
+}
+
 function CheckboxExample() {
 	const [checked, setChecked] = useState(true);
 	return (
@@ -36,6 +54,7 @@ function CheckboxExample() {
 function App() {
 	return (
 		<div className='App'>
+      <ButtonStyled />
       <TextField 
         variant='outlined'
         color='secondary'
